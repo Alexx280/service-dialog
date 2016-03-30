6ec91fc9cfc9c7b265a53b7fbe08e1fc09a6f572
Alexx280 <Alexx280@yandex.ru>
2016-03-30 13:59:19 +0600

    Построение таблици горизонтально

--- otvet.php
+++ otvet.php
@@ -5,54 +5,190 @@
     <style type="text/css">
         table {
             border-collapse: collapse; /* Убираем двойные линии между ячейками */
-            width: 600px; /* Ширина таблицы */
+            width: 2500px; /* Ширина таблицы */
         }
 
-        th {
+        .head_table {
             background: #fc0;
+
+        }
+        tr:nth-child(2n) {
+            background: lightgreen; /* Цвет фона */
         }
     </style>
+
 </head>
 <body>
 
+<form method="post" action="otvet.php">
+    <b>Период проведения опроса</b><br>
+
+    <select name="place" style="width: 244px">
+        <option value="">Выберите из списка</option>
+        <option value="2016-03%">2016 Март</option>
+        <option value="2016-04%">2016 Апрель</option>
+        <option value="2016-05%">2016 Май</option>
+        <option value="2016-06%">2016 Июнь</option>
+        <option value="2016-07%">2016 Июль</option>
+
+
+    </select><br><br>
+    <input type="submit" value="Показать" class="table-form"/><br>
+
+</form>
+<br>
+<script>debugger;</script>
 <?php
-function print_otvety($family1, $name1)
+include_once("funk.php");
+if ($_POST['place'] === '2016-05%') {
+    $data = 'Май 2016г.';
+} else if ($_POST['place'] === '2016-06%') {
+    $data = 'Июнь 2016г.';
+} else if ($_POST['place'] === '2016-07%') {
+    $data = 'Июль 2016г.';
+} else if ($_POST['place'] === '2016-03%') {
+    $data = 'Март 2016г.';
+} else if ($_POST['place'] === '2016-04%') {
+    $data = 'Апрель 2016г.';
+} else {
+};
+
+//echo ('<b>Места инструктирования сотрудников за  '.$data.' </b><br>');
+
+function print_table($name_place)
 {
-    echo('<table border="2"  ><tr ><th colspan="2">' . $family1 . ' ' . $name1 . '</th></tr>');
+    echo('<table border="2"><tr ><th colspan="3">' . $name_place . '</th></tr>');
 
     $link = connect();
     //$login= "SELECT * FROM `lama` WHERE date_test LIKE '".$_POST['place']."' AND `place` = '".$name_place."'";
-    $login = "SELECT * FROM `motiv` WHERE family = '" . $_POST['family1'] . "'";
-    //$login= "SELECT * FROM `motiv` WHERE family = '".$_POST['family1']."' AND name = ".$_POST['name1']."'";
+    $login = "SELECT * FROM `motiv` WHERE date_test LIKE '" . $_POST['place'] . "'";
     $res = $link->query($login);
     $i = 1;
     while ($row = $res->fetch_assoc()) {
+        $family = $row['family'];
+        $name_user = $row['name'];
+        $father_name_user = $row['father_name'];
+        $date_test = $row['date_test'];
+        $job = $row['job'];
+        $first_day = $row['first_day'];
+        echo('<tr>');
+        echo('<td>' . $i . ' <br> </td>');
+        $i = $i + 1;
+        echo('<td width="300">' . $family . ' ' . $name_user . ' ' . $father_name_user . ' <br> </td>');
+        if ($job) {
+            echo('<td>' . $job . ', работает с ' . $first_day . '<br> </td>');
+        } else {
+            echo('<td> Опрос не проводился <br> </td>');
+        }
+        echo('</tr>');
+    };
+
+    echo('</table><br>');
+}
+
+// $i=0;
+// while ($i <4 ) {
+//print_table($names[$i]);
+print_table($data);
+//    $i=$i+1;
+// }
+?>
+
+<form method="post" action="otvet.php">
+    Введите Фамилию и Имя сотрудник для просмотра его ответов: <br>
+    Фамилия: <input type="text" name="family1" size="20"/><br>
+    Имя: <input type="text" name="name1" size="26"/><br>
+
+    <div><input type="submit" value="Показать ответы" class="table-form"/><br></div>
+</form>
+<?php
+
+function print_otvety($family1, $name1)
+{
+    $question = array(
+        "Что такое Стандарт обслуживания на кассе? В чем его цель?",
+        "Какова цель проверок Тайными покупателями?",
+        "Какие пункты Стандарта обслуживания влияют на оценку по итогам проверки?",
+        "Какой балл является максимальным за выполнение СД по новой системе оценки?",
+        "Каким образом рассчитывается оценка по итогам проверки?",
+        "Покупатель первым сказал, что у него есть свой пакет, нужно ли продавцу предлагать пакет покупателю?",
+        "Что является нарушением/невыполнением Сервисного Диалога (при каких условиях ставится ответ «Нет» по какому-либо пункту)?",
+        "Что означает ответ «Не точно» по какому- либо пункту Сервисного Диалога?",
+        "Что означает ответ «-» в анкете?",
+        "Является ли нарушением, если кассир спросил про бонусную карту после того, как назвал сумму покупки?",
+        "Влияет ли на оценку кассира примечание Тайного Покупателя о том, что кассир, например, не поздоровалась с предыдущим или со следующим покупателем?",
+        "Что является основанием для вручения продавцам/кассирам ежегодного звания «Гордость компании»?",
+        "Если вам не понятна какая- либо информация в анкете программа, ваши действия?",
+        "Просматриваете ли вы анкету с подробными ответами и комментариями ТП в программе?",
+        "Какие формы отчетов в модуле ТП вы используете?",
+        "Какие вопросы или сложности в работе модуля ТП у вас возникают?",
+        "Какие сложности у вас возникают при работе с персоналом по итогам проверок ТП?",
+        "Ваши вопросы и пожелания"
+    );
+    echo('<table border="2" ><tr><th rowspan="2" class="head_table">Сотрудник</th> <th colspan="19" class="head_table">Ответы</th></tr>');
+    $link = connect();
+    //$login= "SELECT * FROM `lama` WHERE date_test LIKE '".$_POST['place']."' AND `place` = '".$name_place."'";
+    $login = "SELECT * FROM `motiv` WHERE family = '" . $_POST['family1'] . "'";
+
+    //$login= "SELECT * FROM `motiv` WHERE family = '".$_POST['family1']."' AND name = ".$_POST['name1']."'";
+    $res = $link->query($login);
+
+    $i = 1;
+    while ($row = $res->fetch_assoc()) {
+
         while ($i < 10) {
-            echo('<tr>');
-            echo('<td width="20">' . $i . ' <br> </td>');
-            echo('<td>' . $row['quest0' . $i . ''] . '<br> </td>');
-            echo('</tr>');
+            //
+            echo('<th class="head_table"> ' . $i . ' </th>');
+            //echo('<td width="20">' . $i . ' <br> </td>');
+            //echo('<td width="250">' .  $question[$i-1] . ' <br> </td>');
+            //echo('<td>' . $row['quest0' . $i . ''] . '<br> </td>');
+            //echo('</tr>');
             $i = $i + 1;
         }
         while ($i > 9 && $i < 19) {
-            echo('<tr>');
+            echo('<th class="head_table">' . $i . ' </th>');
+            /*echo('<tr>');
             echo('<td width="20">' . $i . ' <br> </td>');
+            echo('<td width="100">' . $question[$i-1] . ' <br> </td>');
             echo('<td>' . $row['quest' . $i . ''] . '<br> </td>');
-            echo('</tr>');
+            echo('</tr>');*/
             $i = $i + 1;
         }
+
+    };
+    //echo('</table>');
+
+    echo('<tr><th rowspan="2" >'. $_POST['family1'] .' '. $_POST['name1'] .'</th> </tr>');
+    $link = connect();
+    //$login= "SELECT * FROM `lama` WHERE date_test LIKE '".$_POST['place']."' AND `place` = '".$name_place."'";
+    $login = "SELECT * FROM `motiv` WHERE family = '" . $_POST['family1'] . "'";
+
+    //$login= "SELECT * FROM `motiv` WHERE family = '".$_POST['family1']."' AND name = ".$_POST['name1']."'";
+    $res = $link->query($login);
+
+    $i = 1;
+    while ($row = $res->fetch_assoc()) {
+
+        while ($i < 10) {
+            //
+            echo('<th> ' . $row['quest0' . $i . ''] . ' </th>');
+            $i = $i + 1;
+        }
+        while ($i > 9 && $i < 19) {
+            echo('<th>' .  $row['quest' . $i . '']  . ' </th>');
+            $i = $i + 1;
+        }
+
     };
     echo('</table><br>');
+
+
+
 }
 
 print_otvety($_POST['family1'], $_POST['name1']);
 
-echo $row['quest01']
 ?>
-<form method="post" action="otchet.php">
-
-    <input type="submit" value="Обратно" class="table-form"/><br>
-
-</form>
 </body>
 </html>
+
