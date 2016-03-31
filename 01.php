8f9307056987ec55cbe090b737d6937917bec396
Alexx280 <Alexx280@yandex.ru>
2016-03-31 17:56:02 +0600

    Добавлено: защита от перехода на страницу без авторизации

--- 01.php
+++ 01.php
@@ -1,23 +1,49 @@
 <?php
 session_start();
-include_once 'head.tpl';
 include_once "funk.php";
-?>
-<!DOCTYPE HTML>
+include_once "head.tpl";
+if ($_SESSION['id_motiv'] > 1) {
+    echo('
+   <!DOCTYPE HTML>
 <html>
-<div id='centr-q'>
+<div id="centr-q">
     <div id="quest">
         <p class="tq"></p><br>
         Что такое Стандарт обслуживания на кассе? В чем его цель?
-        <form action="<?php echo "0" . (substr(__FILE__, 38, 2) + 1) ?>.php" method="post">
+        <form action="0' . (substr(__FILE__, 38, 2) + 1) . '.php" method="post">
             <br><br><br><br><br><br><br><br>
 
             <p><textarea rows="7" class="text" name="text01"></textarea></p><br><br>
 
             <div style="text-align: center;"><input type="submit" value="Дальше" class="table-form"/><br></div>
         </form>
-        <div class="strings"><p><?php echo(substr(__FILE__, 38, 2)) ?>/18</p></div>
+        <div class="strings"><p>' . (substr(__FILE__, 38, 2)) . '<?php  ?>/18</p></div>
     </div>
 </div>
 
 </html>
+
+    ');
+}
+else if ($_SESSION['id_motiv'] == NULL) {
+
+    echo('
+   <!DOCTYPE HTML>
+<html>
+<div id="centr-q">
+    <div id="quest">
+        <p class="tq">Ой, не хорошо так делать! Введите информацию о себе.</p>
+        <br><br><br>
+        <form action="test.php" method="post">
+            <div style="text-align: center;"><input type="submit" value="Ввести информацию" class="table-form"/><br></div>
+        </form>
+    </div>
+</div>
+
+</html>
+
+    ');
+} else {
+};
+?>
+
