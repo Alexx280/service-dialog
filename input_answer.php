2c829409711d73e3ddb8c0e7d07a7a8a1945a670
Alexx280 <Alexx280@yandex.ru>
2016-04-01 18:01:15 +0600

    убраны лишние файлы

--- input_answer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*if (substr(__FILE__,38,2)+0<10)
-{$chislo = "0".strval(substr(__FILE__,38,2)+0-1);}
-else
-{$chislo = strval(substr(__FILE__,38,2)+0-1);}*/
-$link = connect();
-$q = "UPDATE `motiv` SET `quest" . $chislo . "` = '" . $_POST['text' . $chislo] . "' WHERE `id_motiv`=" . $_SESSION['id_motiv'];
-$res = $link->query($q);
-?>
