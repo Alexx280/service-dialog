16037844764b7cd11f45cd39bb1ddad4b9a9816e
Alexx280 <Alexx280@yandex.ru>
2016-04-20 16:33:00 +0600

    добавлена проверка наличия всех ответов + кнопка "на главную"

--- 20.php
+++ 20.php
@@ -11,8 +11,36 @@ $link = connect();
 $q = "UPDATE `motiv` SET `quest" . $chislo . "` = '" . $_POST['text'] . "' WHERE `id_motiv`=" . $_SESSION['id_motiv'];
 $res = $link->query($q);
 
-$code = "UPDATE `motiv` SET `code` = '" . rand(1000, 9999) . "' WHERE `lama_id`=" . $_SESSION['id_motiv'];
+$code = "UPDATE `motiv` SET `code` = '" . rand(1000, 9999) . "' WHERE `id_motiv`=" . $_SESSION['id_motiv'];
 $res = $link->query($code);
+
+
+$login = "SELECT * FROM `motiv` WHERE id_motiv= '" . $_SESSION['lama_id'] . "'";
+$res = $link->query($login);
+$row = $res->fetch_assoc();
+if ($row['quest01']!= 'NULL' && $row['quest01']!= ' ' &&
+    $row['quest02']!= 'NULL' && $row['quest02']!= ' ' &&
+    $row['quest03']!= 'NULL' && $row['quest03']!= ' ' &&
+    $row['quest04']!= 'NULL' && $row['quest04']!= ' ' &&
+    $row['quest05']!= 'NULL' && $row['quest05']!= ' ' &&
+    $row['quest06']!= 'NULL' && $row['quest06']!= ' ' &&
+    $row['quest07']!= 'NULL' && $row['quest07']!= ' ' &&
+    $row['quest08']!= 'NULL' && $row['quest08']!= ' ' &&
+    $row['quest09']!= 'NULL' && $row['quest09']!= ' ' &&
+    $row['quest10']!= 'NULL' && $row['quest10']!= ' ' &&
+    $row['quest11']!= 'NULL' && $row['quest11']!= ' ' &&
+    $row['quest12']!= 'NULL' && $row['quest12']!= ' ' &&
+    $row['quest13']!= 'NULL' && $row['quest13']!= ' ' &&
+    $row['quest14']!= 'NULL' && $row['quest14']!= ' ' &&
+    $row['quest15']!= 'NULL' && $row['quest15']!= ' ' &&
+    $row['quest16']!= 'NULL' && $row['quest16']!= ' ' &&
+    $row['quest17']!= 'NULL' && $row['quest17']!= ' ' &&
+    $row['quest18']!= 'NULL' && $row['quest18']!= ' ' &&
+    $row['quest19']!= 'NULL' && $row['quest19']!= ' '
+) {$zachet_ok = "UPDATE `motiv` SET `zachet` = 'Зачёт' WHERE `id_motiv`=" . $_SESSION['id_motiv'];}
+else {$zachet_ok = "UPDATE `motiv` SET `zachet` = 'Не зачёт' WHERE `id_motiv`=" . $_SESSION['id_motiv'];}
+$res = $link->query($zachet_ok);
+
 unset ($_SESSION['id_motiv']);
 
 ?>
@@ -30,6 +58,11 @@ unset ($_SESSION['id_motiv']);
 
             Спасибо за Ваши ответы. <br>
         </p>
+        <form method="post" action="index.php">
+            <div id="button-next" style="text-align: center;">
+                <input type="submit" value="На главную" class="table-form"/><br>
+            </div>
+        </form>
 
     </div>
 </div>
